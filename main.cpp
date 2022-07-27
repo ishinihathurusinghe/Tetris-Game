497c1b7ac5c2819f3d4fca8f24eaabd5ecbb8cf6
Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>
2022-07-27 13:48:36 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <SFML/Graphics.hpp                                                                                                   >
+#include <SFML/Graphics.hpp                                                                                                  >
 #include <time.h>
 using namespace sf;
  
